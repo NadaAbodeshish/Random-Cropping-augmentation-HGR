b8d329423c12390d33f953bdad5777cb835a52d9
Nada Aboudeshish <nada.aboudeshish@stud-mail.uni-wuerzburg.de>
2024-12-16 01:00:21 +0100

    augmented shrec data adaptation

--- FPPRC/data/shrec17_dataset.py
+++ FPPRC/data/shrec17_dataset.py
@@ -10,36 +10,49 @@ class SHREC17Dataset(torch.utils.data.Dataset):
         self.num_classes = cfg.DATASETS.SHREC.NUM_CLASSES
         self.inputs_list = self.get_inputs_list(cfg)
         subfolder = cfg.DATASETS.SHREC.PROCESS_FOLDER
-        "../dataset/shrec17/HandGestureDataset_SHREC2017"
-        self.prefix = "../data/shrec17/Processed_HandGestureDataset_SHREC2017/"  + subfolder +  "/gesture_{}/finger_{}/subject_{}/essai_{}"
+        self.prefix = "../data/shrec17/Processed_HandGestureDataset_SHREC2017/" + subfolder + "/gesture_{}/finger_{}/subject_{}/essai_{}"
         self.r = re.compile('[ \t\n\r:]+')
+
+        # Augmented dataset options
+        self.augmented_files = ['original_pts_label.npy', 'aug_1_pts_label.npy', 'aug_2_pts_label.npy', 'aug_3_pts_label.npy']
 
     def __getitem__(self, index):
         splitLine = self.r.split(self.inputs_list[index])
         label28 = int(splitLine[-3]) - 1
         label14 = int(splitLine[-4]) - 1
-        input_data = np.float32(np.load(
-            self.prefix.format(splitLine[0], splitLine[1], splitLine[2], splitLine[3])
-            + "/pts_label.npy"))[:, :, :7]
 
-        pcd = input_data[:, :, 4:7]
+        # Randomly select an augmented file if training
+        if self.phase == 'train':
+            selected_file = np.random.choice(self.augmented_files)
+        else:
+            selected_file = 'original_pts_label.npy'  # Use original data for validation/testing
+
+        # Load the point cloud data
+        input_data_path = (
+            self.prefix.format(splitLine[0], splitLine[1], splitLine[2], splitLine[3])
+            + f"/{selected_file}"
+        )
+        input_data = np.float32(np.load(input_data_path))[:, :, :7]
+
+        pcd = input_data[:, :, 4:7]  # Extract 3D point cloud (x, y, z)
 
         if self.phase == 'train':
             T, N, D = pcd.shape
             pcd = pcd.reshape(-1, D)
-            pcd -= np.mean(pcd, axis=0)
-            pcd, rot_angle = self.rotate(pcd)
-            pcd, scale_value = self.scale(pcd)
+            pcd -= np.mean(pcd, axis=0)  # Normalize
+            pcd, rot_angle = self.rotate(pcd)  # Apply rotation
+            pcd, scale_value = self.scale(pcd)  # Apply scaling
             pcd = pcd.reshape(T, N, D)
 
+        # Separate for local and global streams
         pcd_loc = pcd.copy()
         pcd_glob = pcd.copy()
 
-        # mean-center each frame separately for local stream
+        # Mean-center each frame separately for local stream
         means_per_frame = np.mean(pcd_loc, axis=1)
         pcd_loc -= means_per_frame[:, None, :]
 
-        # mean-center the whole sequence for global stream
+        # Mean-center the whole sequence for global stream
         T, N, D = pcd_glob.shape
         pcd_glob = pcd_glob.reshape(-1, D)
         pcd_glob -= np.mean(pcd_glob, axis=0)
@@ -47,12 +60,15 @@ class SHREC17Dataset(torch.utils.data.Dataset):
 
         input_data = [pcd_loc, pcd_glob]
 
+        # Set the correct label
         if self.num_classes == 28:
             label = label28
         elif self.num_classes == 14:
             label = label14
         else:
-            raise ValueError("Shrec Dataset only provides 14 or 28 label classes, not {} as specified".format(self.num_classes))
+            raise ValueError(
+                "Shrec Dataset only provides 14 or 28 label classes, not {} as specified".format(self.num_classes)
+            )
 
         return input_data, label, self.inputs_list[index]
 
@@ -65,7 +81,7 @@ class SHREC17Dataset(torch.utils.data.Dataset):
         elif self.phase == "test":
             file_list = cfg.DATASETS.FILE_LIST_TEST
         else:
-            raise ValueError
+            raise ValueError("Phase should be 'train', 'val', or 'test'")
         inputs_path = os.path.join(prefix, file_list)
         inputs_list = open(inputs_path).readlines()
         return inputs_list
