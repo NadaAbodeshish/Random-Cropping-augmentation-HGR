2d35a1c29d5b9a16988ca0d9ad209df8f3166d2d
Nada Aboudeshish <nada.aboudeshish@stud-mail.uni-wuerzburg.de>
2025-01-19 16:06:51 +0100

    fastai LR handling enviroment change

--- e2eET/experiments.server/_functionsClasses.py
+++ e2eET/experiments.server/_functionsClasses.py
@@ -17,7 +17,7 @@ from datetime import datetime
 import torch
 import torch.nn as NN
 import numpy as np
-from fastai.callback.progress import ConsoleProgressBar
+from fastprogress.fastprogress import fastprogress
 from fastai.vision.all import *
 from fastai.vision.all import aug_transforms, Resize, Normalize, imagenet_stats, GrandparentSplitter
 from fastai.torch_core import defaults
@@ -366,7 +366,8 @@ import traceback
 
         
 def i_LRFinder(learn, show_plot=False, n_attempts=0):
-    try: return learn.lr_find(suggest_funcs=(valley, slide), show_plot=show_plot, cbs=[ConsoleProgressBar()])
+    fastprogress.printing = True
+    try: return learn.lr_find(suggest_funcs=(valley, slide), show_plot=show_plot)
 
 
     except Exception as e:
